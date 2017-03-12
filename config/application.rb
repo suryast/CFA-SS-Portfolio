bdee14e70e0543f23860ea4a7faf5fa6ce88c11d
Surya Setiyaputra <hello@setiyaputra.me>
2017-03-13 06:42:34 +1100

    updated applications.rb to try fixing error in heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,5 +13,6 @@ module Web
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
   end
 end
